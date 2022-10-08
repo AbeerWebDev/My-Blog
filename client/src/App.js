6f08733af5a42b7bc149d7632e366619e833c57f
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-10-08 20:15:53 +0400

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Single from './pages/Single'
+import Write from './pages/Write'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/single",
+    element: <Single />,
+  },
+  {
+    path: "/write",
+    element: <Write />,
+  }
+]);
+
+function App() {
+  return (
+    <div>
+     <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
