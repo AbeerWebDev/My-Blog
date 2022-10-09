fe795f5c96fc0c4775745c728876be0db542efa4
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-10-09 13:57:12 +0400

    Added components

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,45 @@ import {
   createBrowserRouter,
   RouterProvider,
   Route,
+  Outlet,
 } from "react-router-dom";
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Single from './pages/Single'
 import Write from './pages/Write'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import './style.scss'
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ]
   },
   {
     path: "/login",
@@ -22,20 +50,14 @@ const router = createBrowserRouter([
     path: "/register",
     element: <Register />,
   },
-  {
-    path: "/single",
-    element: <Single />,
-  },
-  {
-    path: "/write",
-    element: <Write />,
-  }
 ]);
 
 function App() {
   return (
-    <div>
-     <RouterProvider router={router} />
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
