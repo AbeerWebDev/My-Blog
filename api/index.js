9f610fd1ea2672a002081d21f8568a6529157d88
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-10-12 19:24:49 +0400

    Updated auth.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 import express from "express";
+import postRoutes from './routes/posts.js'
+import usersRoutes from './routes/users.js'
+import authRoutes from './routes/auth.js'
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use('/api/posts', postRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/users', usersRoutes)
 
 app.listen(8800, ()=>{
     console.log("Connected to backend!")
-})
+});
