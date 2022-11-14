ddc6ccfa2e454b67ede1bd858dc5ba0ddc52426b
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-11-15 00:55:44 +0400

    Update db.js

--- api/db.js
+++ api/db.js
@@ -3,18 +3,9 @@
 
 dotenv.config()
 
-//  export const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: "root",
-//     password: process.env.DB_KEY,
-//     database: "blog"
-//  })
-
-
-
  export const db = mysql.createConnection({
-    host: process.env.HOST ?? 'localhost',
-    user: process.env.USER ?? "root",
+    host: 'localhost',
+    user: "root",
     password: process.env.DB_KEY,
-    database: process.env.DATABASE ?? "blog"
+    database: "blog"
  })
