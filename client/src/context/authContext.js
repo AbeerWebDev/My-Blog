f19fc4e4f99cc48a25a51234d065f2da8698d0fc
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-11-26 13:23:17 +0400

    Update authContext.js

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -8,12 +8,12 @@ export const AuthContextProvider = ({children}) => {
     const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("user")) || null)
 
     const login = async (inputs) => {
-       const res = await axios.post(`${process.env.REACT_APP_API}/auth/login`, inputs);
+       const res = await axios.post("/auth/login", inputs);
        setCurrentUser(res.data)
     }
 
     const logout = async (inputs) => {
-       await axios.post(`${process.env.REACT_APP_API}/auth/logout`);
+       await axios.post("/auth/logout");
        setCurrentUser(null)
     }
 
@@ -26,3 +26,4 @@ export const AuthContextProvider = ({children}) => {
     )
 } 
 
+//${process.env.REACT_APP_API}
